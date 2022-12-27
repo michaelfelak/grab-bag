717fa728f0da001ab3ae9feea1c618ace355db78
Michael Felak <michaelfelak@gmail.com>
2022-12-27 09:54:07 -0500

    Auto-generated commit [ci skip]

--- src/app/shared/models/entry.model.ts
+++
@@ -1,9 +0,0 @@
-import { Person } from './person.model';
-
-export interface GrabBagEntry {
-  key: string;
-  year: number;
-  giver?: Person;
-  receiver?: Person;
-  notes?: string;
-}
--- src/app/shared/models/person.model.ts
+++
@@ -1,5 +0,0 @@
-export interface Person {
-  first_name?: string;
-  last_name?: string;
-  maiden_name?: string;
-}
